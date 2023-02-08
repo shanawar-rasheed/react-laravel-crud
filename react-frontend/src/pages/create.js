4a0c39b25eeb17d63d4200d9beb8a8e009b30604
Shanawar Rasheed <80325239+shanawar-rasheed@users.noreply.github.com>
2023-02-08 23:28:01 +0500

    user-creeateion

--- react-frontend/src/pages/create.js
+++ react-frontend/src/pages/create.js
@@ -1,3 +1,63 @@
-export default function create() {
-  return <div>Create Page</div>;
+import http from "../http";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Create() {
+  const navigate = useNavigate ();
+  const [inputs, setInputs] = useState({});
+  const handleChanges = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  const submitForm = () => {
+    http.post("/users", inputs).then((res) => {
+      navigate("/");
+    });
+  };
+
+  return (
+    <div>
+      <h2>Add User</h2>
+      <div className="row">
+        <div className="col-md-6">
+          <div className="card p-4">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              className="form-control mb-2"
+              value={inputs.name || ""}
+              onChange={handleChanges}
+            />
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className=" form-control mb-2"
+              value={inputs.email || ""}
+              onChange={handleChanges}
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className=" form-control mb-2"
+              value={inputs.password || ""}
+              onChange={handleChanges}
+            />
+            <input
+              type="button"
+              value="submit"
+              className="btn btn-success mb-2"
+              onClick={submitForm}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- react-frontend/src/pages/home.js
+++ react-frontend/src/pages/home.js
@@ -12,5 +12,31 @@ export default function Home() {
       setUsers(res.data);
     });
   };
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <h2>User Listing</h2>
+      <div >
+        <table className="table">
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>edit</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
